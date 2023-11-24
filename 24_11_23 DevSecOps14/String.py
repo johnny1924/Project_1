c22b87514742caacd4f7a6f17cbe6d6a1c0119e6
johnny halevi <yonnihalevi@gmail.com>
2023-11-24 13:14:01 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ 24_11_23 DevSecOps14/String.py
@@ -0,0 +1,24 @@
+# #       0123454678
+# name = 'yaron chen'
+#
+# print(name)
+# print(name[0:3])
+# print(name[5])
+# print(name[-4:-1])
+# print(name[:-4])
+# print(name[-4:])
+
+# ip = '192.168.250.11/16'
+#
+# print(ip[:-3]) #ip
+# print(ip[-2:]) #subnet
+
+
+# credit_card = '4580 0900 3344 8481'
+# print (credit_card[-4:])
+
+# sen = 'can i be any more late ?'
+# # print(len(sen))
+# # print(sen.upper()) # --uppercase
+# # print(sen.lower()) # --lowercase
+# # print(sen.title()) # --first letter cap
---
+++ 24_11_23 DevSecOps14/main.py
@@ -0,0 +1,60 @@
+# # print('hello world')
+#
+# #
+# print(3+3)
+# print('3+3')
+# print(True)
+# print(100.1)
+
+
+# x = 17
+# y = 3
+#
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y) # / -> float
+# print(x//y) # // -> int
+# print(x%y)
+# print(x**y)
+# print(144**0.5)
+
+# ==================assign====================
+# a=4
+# b=6
+# a,b=b,a #swaping
+# print('a', a)
+# print('b', b)
+
+# counter = 0
+# print(counter)
+# counter = counter+1
+#
+# counter +=1
+#
+# print(counter)
+# counter = counter+1
+#
+# counter+=1
+#=================casting===============
+
+# x=4
+# x = str(x)
+# print('x'-3)
+
+
+# number1 = '-19.5'
+#
+# number1 = int(float(number1))
+#
+# print(number1)
+
+#=============================input=============================
+
+name = input('please enter a name : ')
+print(name)
+print(type(name))
+
+age = int(input('please enter your age :'))
+print(age-1)
+print(type(age))
